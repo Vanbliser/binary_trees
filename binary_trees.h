4b36cd39f679a99ebb557117dbfdcea6406bced3
Blossom Ayogu <ayogublossomisrael@gmail.com>
2023-08-02 10:36:32 +0000

    Add function prototypes

--- binary_trees.h
+++ binary_trees.h
@@ -85,5 +85,17 @@ size_t binary_tree_nodes(const binary_tree_t *tree);
 int binary_tree_balance(const binary_tree_t *tree);
 int get_binary_tree_height(const binary_tree_t *tree);
 
+/* 15-binary_tree_is_full.c */
+int binary_tree_is_full(const binary_tree_t *tree);
+
+/* 16-binary_tree_is_perfect.c */
+int binary_tree_is_perfect(const binary_tree_t *tree);
+size_t binary_tree_height(const binary_tree_t *tree);
+
+/* 17-binary_tree_sibling.c */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
+
+/* 18-binary_tree_uncle.c */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 
 #endif /* BINARY_TREE_H */
