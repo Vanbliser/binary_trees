f4bc6f265766f623ed43617f09570004f09a22a1
Blossom Ayogu <ayogublossomisrael@gmail.com>
2023-08-02 00:49:49 +0000

    Update codebase

--- 2-main.c
+++ 2-main.c
@@ -19,6 +19,5 @@ int main(void)
 	binary_tree_insert_right(root->left, 54);
 	binary_tree_insert_right(root, 128);
 	binary_tree_print(root);
-	binary_tree_delete(root);
 	return (0);
 }
--- binary_trees.h
+++ binary_trees.h
@@ -1,10 +1,7 @@
 #ifndef BINARY_TREE_H
 #define BINARY_TREE_H
 
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-
 
 /* Data structures */
 
